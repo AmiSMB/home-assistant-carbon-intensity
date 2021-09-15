8abefd602d05760f41734456fa61a5d54c1e8cf5
Grant Fribbens <gfribbens@lemongrassconsulting.com>
2021-09-15 21:47:42 +0100

    Update for HACS

---
+++ custom_components/great_britain_carbon_intensity/__init__.py
@@ -0,0 +1,6 @@
+"""
+Custom integration to integrate great_britain_carbon_intensity with Home Assistant.
+
+For more details about this integration, please refer to
+https://github.com/AmiSMB/home-assistant-carbon-intensity
+"""
